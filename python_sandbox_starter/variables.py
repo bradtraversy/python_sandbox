0e113331eb5df959ee5e6be932c38e47f59e547d
breakthatbass <gamache.taylor@gmail.com>
2019-04-25 09:14:30 -0400

    add some variables

--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -13,3 +13,20 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+'''
+for running code: python3 variables.py
+
+x = 1           # int
+y = 2.5         # float
+name = 'John'   # str
+is_cool = True  # bool
+
+'''
+
+# Multiple assignment
+
+x, y, name, is_cool = (1, 2.5, 'John', True)
+
+print('Hello')
+
