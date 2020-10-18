6a2ab298891559482609d3b29a59400fb19239f9
kalyaniingle7387 <73074957+kalyaniingle7387@users.noreply.github.com>
2020-10-18 23:16:46 +0530

    Add official documentation link

--- python_sandbox_starter/dictionaries.py
+++ python_sandbox_starter/dictionaries.py
@@ -1,1 +1,2 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
+# Read more about dictionaries at https://docs.python.org/3/tutorial/datastructures.html#dictionaries
